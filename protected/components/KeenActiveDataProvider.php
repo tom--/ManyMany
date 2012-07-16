1c702e8552b7ce341acfd8f5acbf6cbdfb123002
tom-- <fsb@thefsb.org>
2012-07-16 15:42:16 -0400

    Added KeenActiveDataProvider signature and doc block.

---
+++ protected/components/KeenActiveDataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+class KeenActiveDataProvider extends CActiveDataProvider {
+	/**
+	 * Preload models into $data.
+	 *
+	 * @param mixed $relations Specifies the relations to be preloaded. If a string,
+	 * the name of a relation to preload. If an array of strings, a list of relations as
+	 * specified for {@link CActiveRecord:relations()}.
+	 */
+	public function preload($relations) {
+
+	}
+}
