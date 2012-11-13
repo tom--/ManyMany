63de624bea56427c1ae3376cfc421de1b018e8d8
tom-- <fsb@thefsb.org>
2012-11-13 14:19:44 -0500

    style

--- protected/controllers/SongController.php
+++ protected/controllers/SongController.php
@@ -1,150 +1,180 @@
 <?php
 
-class SongController extends Controller {
-	public $layout = '//layouts/column2';
+class SongController extends Controller
+{
+    public $layout = '//layouts/column2';
 
-	public function filters() {
-		return array(
-		);
-	}
+    public function filters()
+    {
+        return array();
+    }
 
-	public function accessRules() {
-	}
+    public function accessRules()
+    {
+    }
 
-	public function actionView($id) {
-		$this->render('view', array(
-			'model' => $this->loadModel($id),
-		));
-	}
+    public function actionView($id)
+    {
+        $this->render(
+            'view',
+            array(
+                'model' => $this->loadModel($id),
+            )
+        );
+    }
 
-	protected function searchSong($scenario = '') {
-		$song = new Song($scenario);
-		$song->unsetAttributes();
-		if (isset($_GET['Song'])) {
-			$song->attributes = $_GET['Song'];
-		}
-		return $song;
-	}
+    protected function searchSong($scenario = '')
+    {
+        $song = new Song($scenario);
+        $song->unsetAttributes();
+        if (isset($_GET['Song'])) {
+            $song->attributes = $_GET['Song'];
+        }
+        return $song;
+    }
 
-	/**
-	 * Set up models with CGV search form input.
-	 *
-	 * @param CActiveRecord $model
-	 */
-	protected function setSearchInputs($model) {
-		foreach (array('Reviewer', 'Review', 'Song', 'SongGenre', 'Genre') as $class) {
-			if (get_class($model) === $class) {
-				$model->unsetAttributes();
-				if (isset($_GET[$class])) {
-					$model->attributes = $_GET[$class];
-				}
-			} else {
-				$prop = 'search' . $class;
-				if (property_exists($model, $prop)) {
-					$model->$prop = new $class('search');
-					$model->$prop->unsetAttributes();
-					if (isset($_GET[$class])) {
-						$model->$prop->attributes = $_GET[$class];
-					}
-				}
-			}
-		}
-	}
+    /**
+     * Set up models with CGV search form input.
+     *
+     * @param CActiveRecord $model
+     */
+    protected function setSearchInputs($model)
+    {
+        foreach (array('Reviewer', 'Review', 'Song', 'SongGenre', 'Genre') as $class) {
+            if (get_class($model) === $class) {
+                $model->unsetAttributes();
+                if (isset($_GET[$class])) {
+                    $model->attributes = $_GET[$class];
+                }
+            } else {
+                $prop = 'search' . $class;
+                if (property_exists($model, $prop)) {
+                    $model->$prop = new $class('search');
+                    $model->$prop->unsetAttributes();
+                    if (isset($_GET[$class])) {
+                        $model->$prop->attributes = $_GET[$class];
+                    }
+                }
+            }
+        }
+    }
 
-	/**
-	 * Grid of all songs including genres column
-	 */
-	public function actionSongs() {
-		$song = new Song('search');
-		$this->setSearchInputs($song);
-		$this->render('songsGrid', array(
-			'song' => $song,
-		));
-	}
+    /**
+     * Grid of all songs including genres column
+     */
+    public function actionSongs()
+    {
+        $song = new Song('search');
+        $this->setSearchInputs($song);
+        $this->render(
+            'songsGrid',
+            array(
+                'song' => $song,
+            )
+        );
+    }
 
-	/**
-	 * Grid of all song reviews
-	 */
-	public function actionReviews() {
-		$review = new Review('search');
-		$review->unsetAttributes();
-		$song = new Song('search');
-		$song->unsetAttributes();
-		$genre = new Genre('search');
-		$genre->unsetAttributes();
+    /**
+     * Grid of all song reviews
+     */
+    public function actionReviews()
+    {
+        $review = new Review('search');
+        $review->unsetAttributes();
+        $song = new Song('search');
+        $song->unsetAttributes();
+        $genre = new Genre('search');
+        $genre->unsetAttributes();
 
-		if (isset($_GET['Review']))
-			$review->attributes = $_GET['Review'];
-		if (isset($_GET['Song']))
-			$song->attributes = $_GET['Song'];
-		if (isset($_GET['Genre']))
-			$genre->attributes = $_GET['Genre'];
+        if (isset($_GET['Review'])) {
+            $review->attributes = $_GET['Review'];
+        }
+        if (isset($_GET['Song'])) {
+            $song->attributes = $_GET['Song'];
+        }
+        if (isset($_GET['Genre'])) {
+            $genre->attributes = $_GET['Genre'];
+        }
 
-		$review->searchSong = $song;
-		$review->searchGenre = $genre;
+        $review->searchSong = $song;
+        $review->searchGenre = $genre;
 
-		if (!isset($_GET['ajax']))
-			$this->render('reviewsGrid', array(
-				'review' => $review,
-				'song' => $song,
-				'genre' => $genre,
-			));
-		elseif ($_GET['ajax'] === 'song-grid')
-			$this->renderPartial('_reviewsGrid1', array(
-				'review' => $review,
-				'song' => $song,
-				'genre' => $genre,
-			));
-		elseif ($_GET['ajax'] === 'song-grid-2')
-			$this->renderPartial('_reviewsGrid2', array(
-				'review' => $review,
-				'song' => $song,
-				'genre' => $genre,
-			));
-		elseif ($_GET['ajax'] === 'song-grid-3')
-			$this->renderPartial('_reviewsGrid3', array(
-				'review' => $review,
-				'song' => $song,
-				'genre' => $genre,
-			));
-	}
+        if (!isset($_GET['ajax'])) {
+            $this->render(
+                'reviewsGrid',
+                array(
+                    'review' => $review,
+                    'song' => $song,
+                    'genre' => $genre,
+                )
+            );
+        } elseif ($_GET['ajax'] === 'song-grid') {
+            $this->renderPartial(
+                '_reviewsGrid1',
+                array(
+                    'review' => $review,
+                    'song' => $song,
+                    'genre' => $genre,
+                )
+            );
+        } elseif ($_GET['ajax'] === 'song-grid-2') {
+            $this->renderPartial(
+                '_reviewsGrid2',
+                array(
+                    'review' => $review,
+                    'song' => $song,
+                    'genre' => $genre,
+                )
+            );
+        } elseif ($_GET['ajax'] === 'song-grid-3') {
+            $this->renderPartial(
+                '_reviewsGrid3',
+                array(
+                    'review' => $review,
+                    'song' => $song,
+                    'genre' => $genre,
+                )
+            );
+        }
+    }
 
-	/*
-	public function actionAddReviews() {
-		$nSongs = Song::model()->count() - 1;
-		$reviewers = Reviewer::model()->findAll();
-		foreach ($reviewers as $reviewer) {
-			$nreviews = mt_rand(0, 4);
-			if ($nreviews) {
-				for ($i = 0; $i < $nreviews; $i += 1) {
-					$review = new Review;
-					$review->reviewer_id = $reviewer->id;
-					$review->review = Lipsum::getLipsum(mt_rand(1,3));
-					$row = mt_rand(0, $nSongs);
-					$review->song_id = Yii::app()->db->createCommand(
-						"select id from song limit $row, 1"
-					)->queryScalar();
-					$review->save();
-				}
-			}
-		}
-		$this->redirect(array('admin'));
-	}
-	*/
+    /*
+    public function actionAddReviews() {
+        $nSongs = Song::model()->count() - 1;
+        $reviewers = Reviewer::model()->findAll();
+        foreach ($reviewers as $reviewer) {
+            $nreviews = mt_rand(0, 4);
+            if ($nreviews) {
+                for ($i = 0; $i < $nreviews; $i += 1) {
+                    $review = new Review;
+                    $review->reviewer_id = $reviewer->id;
+                    $review->review = Lipsum::getLipsum(mt_rand(1,3));
+                    $row = mt_rand(0, $nSongs);
+                    $review->song_id = Yii::app()->db->createCommand(
+                        "select id from song limit $row, 1"
+                    )->queryScalar();
+                    $review->save();
+                }
+            }
+        }
+        $this->redirect(array('admin'));
+    }
+    */
 
-	/**
-	 * Returns a Song model given its primary key.
-	 *
-	 * @param integer $id the ID of the Song to be loaded
-	 * @throws CHttpException If the Song model is not found.
-	 * @return CActiveRecord
-	 */
-	public function loadModel($id) {
-		$song = Song::model()->findByPk($id);
-		if ($song === null) {
-			throw new CHttpException(404, 'The requested page does not exist.');
-		}
-		return $song;
-	}
-
+    /**
+     * Returns a Song model given its primary key.
+     *
+     * @param integer $id the ID of the Song to be loaded
+     *
+     * @throws CHttpException If the Song model is not found.
+     * @return CActiveRecord
+     */
+    public function loadModel($id)
+    {
+        $song = Song::model()->findByPk($id);
+        if ($song === null) {
+            throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $song;
+    }
 }
--- protected/views/song/_reviewsGrid1.php
+++ protected/views/song/_reviewsGrid1.php
@@ -9,7 +9,6 @@
 ######################################################################
 //using the search() method for the 1st usecase
 
-
 $columns = array(
     array(
         'header' => '',
@@ -21,73 +20,70 @@ $columns = array(
             'style' => 'display:none',
         ),
     ),
-	array(
-		'header' => 'Num',
-		'value' => Help::$gridRowExp,
-	),
-	array(
-		'name' => 'review',
+    array(
+        'header' => 'Num',
+        'value' => Help::$gridRowExp,
+    ),
+    array(
+        'name' => 'review',
         'htmlOptions' => array(
             'contenteditable' => 'true',
         ),
         'cssClassExpression' => '"review"',
-	),
-	array(
-		'name' => 'song.name',
-		'filter' => CHtml::activeTextField($song, 'name'),
-	),
-	array(
-		'name' => 'song.artist',
-		'filter' => CHtml::activeTextField($song, 'artist'),
-	),
-	array(
-		'name' => 'song.album',
-		'filter' => CHtml::activeTextField($song, 'album'),
-	),
-	array(
-		'name' => 'genres.name',
-		'type' => 'raw',
-		'header' => 'Genres',
-		'value' => 'Help::tags($data->song->genreNames, "genres", true)',
-		'filter' => CHtml::activeTextField($genre, 'name'),
-		//'filter' => CHtml::activedropDownList($genre, 'id', CHtml::listData(Genre::model()->findAll(array('order'=>'name')),'id','name'), array('empty'=>'Select') ),
-	),
+    ),
+    array(
+        'name' => 'song.name',
+        'filter' => CHtml::activeTextField($song, 'name'),
+    ),
+    array(
+        'name' => 'song.artist',
+        'filter' => CHtml::activeTextField($song, 'artist'),
+    ),
+    array(
+        'name' => 'song.album',
+        'filter' => CHtml::activeTextField($song, 'album'),
+    ),
+    array(
+        'name' => 'genres.name',
+        'type' => 'raw',
+        'header' => 'Genres',
+        'value' => 'Help::tags($data->song->genreNames, "genres", true)',
+        'filter' => CHtml::activeTextField($genre, 'name'),
+        //'filter' => CHtml::activedropDownList($genre, 'id', CHtml::listData(Genre::model()->findAll(array('order'=>'name')),'id','name'), array('empty'=>'Select') ),
+    ),
 );
-
 
 $dp = $review->search();
 $dp->pagination->pageSize = 5;
 $grid = array(
-	'id' => 'song-grid',
-	'dataProvider' => $dp,
-	'filter' => $review,
-	'columns' => $columns,
+    'id' => 'song-grid',
+    'dataProvider' => $dp,
+    'filter' => $review,
+    'columns' => $columns,
 );
 
 //	lazy loading all SongGenre data at once
 $songIds = array();
 foreach ($dp->data as $review) {
-	$songIds[] = $review->song_id;
+    $songIds[] = $review->song_id;
 }
 $songIds = array_unique($songIds);
 if ($songIds) {
-	$dpSongGenres = SongGenre::model()->with('genre')->findAllByAttributes(
-		array('song_id' => $songIds)
-	);
+    $dpSongGenres = SongGenre::model()->with('genre')->findAllByAttributes(
+        array('song_id' => $songIds)
+    );
 }
 
 //	putting the SongGenre's in the right place
-foreach($dp->data as $review)
-{
-	$hasGenres = array();
-	foreach($dpSongGenres as $songGenre)
-	{
-		if($songGenre->song_id===$review->song_id)
-			$hasGenres[] = $songGenre;
-	}
-	$review->song->hasGenres = $hasGenres;
+foreach ($dp->data as $review) {
+    $hasGenres = array();
+    foreach ($dpSongGenres as $songGenre) {
+        if ($songGenre->song_id === $review->song_id) {
+            $hasGenres[] = $songGenre;
+        }
+    }
+    $review->song->hasGenres = $hasGenres;
 }
-
 
 echo CHtml::tag('h2', array(), 'First usecase: Manage ' . $this->action->id);
 $this->widget('zii.widgets.grid.CGridView', $grid);
